59b85afc68c3649e4af4bf44c5e24b0a439778ce
shiorinakayama <m0116225f2@edu.teu.ac.jp>
2018-09-03 09:26:32 +0900

    バー上に数値あり

--- app/src/main/java/com/example/shiorinakayama/moneybook_0621/MainActivity.java
+++ app/src/main/java/com/example/shiorinakayama/moneybook_0621/MainActivity.java
@@ -17,6 +17,8 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 import android.util.Log;
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,16 +35,18 @@ public class MainActivity extends AppCompatActivity {
 
     class MyView extends View {
         private Paint paint;
-        private float xRect = 0;
-        private float xRect2 = 0;
+        private short xRect = 0;
+        private short xRect2 = 0;
         private float xRect3 = 0;
         private float yRect = 0;
         public boolean x = false;
         private final String TAG = "Square_1";
         private float rest;
         private float a;
+        private float b;
 
-        final TextView textView = (TextView)findViewById(R.id.all);
+
+
 
 
 
@@ -53,11 +57,21 @@ public class MainActivity extends AppCompatActivity {
             yRect = event.getY();
 
             if (x){
-                xRect = event.getX();
+                xRect = (short)event.getX();
             }else{
-                xRect2 = event.getX();
+                xRect2 = (short)event.getX();
             }
             Log.d("TouchEvent","Y:"+yRect);
+
+            switch (event.getAction()){
+                case MotionEvent.ACTION_UP:
+
+                    break;
+            }
+
+
+
+
 
 
             this.invalidate();
@@ -75,20 +89,44 @@ public class MainActivity extends AppCompatActivity {
         protected void onDraw(Canvas canvas){
             //背景
             canvas.drawARGB(255,255,255,255);
+
+            Bitmap out = BitmapFactory.decodeResource(getResources(),R.drawable.out64);
+            Bitmap  in = BitmapFactory.decodeResource(getResources(),R.drawable.in64);
+
+            //黄色円
+            paint.setARGB(255,255,255,137);
+            paint.setStyle(Paint.Style.FILL_AND_STROKE);
+            canvas.drawArc(120,10,970,800,0,0 + xRect2/3,true,paint);
+
+
+
+            //緑の円
             paint.setARGB(255,132,255,193);
             paint.setAntiAlias(true);
             paint.setStyle(Paint.Style.FILL_AND_STROKE);
 
-            Bitmap eat = BitmapFactory.decodeResource(getResources(),R.drawable.eating);
-            Bitmap shirt = BitmapFactory.decodeResource(getResources(),R.drawable.shirt);
-            //緑の円
             canvas.drawCircle(550,400,350, paint);
+
+
+
             //白い円
             paint.setARGB(255, 255, 255, 255);
             paint.setStyle(Paint.Style.FILL_AND_STROKE);
             canvas.drawCircle(550, 400, 170, paint);
             //白い弧
-            canvas.drawArc(150,40,1050,850,0,0 - xRect/5 - xRect2/5,true,paint);
+            canvas.drawArc(130,20,1050,850,0,0 + a ,true,paint);
+
+            if (xRect >= 1079){
+                a = 360;
+
+            }else{
+                a = - 1*xRect/3;
+            }
+
+            //ピンク短形（右）
+            /*paint.setColor(Color.argb(255, 255, 142, 198));
+            paint.setStyle(Paint.Style.FILL_AND_STROKE);
+            canvas.drawRect(860 + xRect3, 1200 , 1080, 1450, paint);**/
 
 
 
@@ -96,33 +134,50 @@ public class MainActivity extends AppCompatActivity {
             paint.setColor(Color.argb(255, 255, 142, 198));
             paint.setStyle(Paint.Style.FILL_AND_STROKE);
             canvas.drawRect(0 , 1200 , 220 + xRect2, 1450, paint);
+            canvas.drawBitmap(in,20 + xRect2,1230,paint);
            // Log.d("onTouchEvent","ピンクを引っ張っている長さは「"+ 180+xRect+"」");
 
 
 
             //アイコン
-            //canvas.drawBitmap(eat,500 + xRect,1300,paint);
+
 
             //青短径
             paint.setColor(Color.argb(255, 147, 201, 255));
             paint.setStyle(Paint.Style.FILL_AND_STROKE);
             canvas.drawRect(0, 850, 220+xRect , 1100, paint);
-            //canvas.drawBitmap(shirt,0 ,1050,paint);
+            canvas.drawBitmap(out,20 + xRect,880,paint);
 
             //テキスト
             Paint textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
             textPaint.setTextSize(50);
 
-            if(yRect >= 1200){
+            if(yRect >= 1400){
                 x = false;
 
-            }else{
+            }else if (yRect <= 1230){
                 x = true;
             }
-            rest = xRect;
+            rest =1500 - xRect + xRect2;
+
 
             String text = ""+rest+"";
+
             canvas.drawText(text,460,420,textPaint  );
+
+            //青テキスト
+            paint.setARGB(255,147,201,255);
+            paint.setTextSize(90);
+
+            String btext = "" + xRect + "";
+            canvas.drawText(btext,200 + xRect,860,paint);
+
+            //赤テキスト
+            paint.setARGB(255,255,142,198);
+            paint.setTextSize(90);
+            String rtext = "" + xRect2 + "";
+            canvas.drawText(rtext,200 + xRect2,1210,paint);
+
 
 
 
